aae7c9fa8de46b73f1715d79721065963c6e5bd7
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2025-02-12 07:29:09 +0530

    Add files via upload

---
+++ databse/db.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'yourWestminsterID_0';
+//create a DB connection 
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+//if the DB connection fails, display an error message and exit
+if (!$conn)
+{
+ die('Could not connect: ' . mysqli_error($conn));
+}
+//select the database
+mysqli_select_db($conn, $dbname);
+?>
+</body>
+</html>
